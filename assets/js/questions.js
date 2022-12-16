d8989b650716902d148cf3d1ee439b7511990db4
Suzan Dewitz <suzanyaa@yahoo.com>
2022-12-16 09:01:49 +0000

    Add code to in question quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,70 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Which is the part of the computer system that one can physically touch?",
+        answer: "software",
+        options: [
+            "data",
+            "hardware",
+            "software",
+            "operating systems"
+        ]
+    },
+    {
+        numb: 2,
+        question: "IC chips used in computers are usually made of?",
+        answer: "Silicon",
+        options: [
+            "Gold",
+            "Lead",
+            "Chromium",
+            "Silicon"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is not an example of an Operating System?",
+        answer: "Microsoft Office XP",
+        options:[
+            "Microsoft Office XP",
+            "Red Hat Linux",
+            "BSD Unix",
+            "Windows 98"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is not an input device?",
+        answer: "VDU",
+        options:[
+            "Mouse",
+            "Light pen",
+            "Keyboard",
+            "VDU"
+        ]
+    },
+    {
+        numb: 5,
+        question: "An electronic device that process data, converting it into information?",
+        answer: "computer",
+        options: [
+            "computer",
+            "processor",
+            "case",
+            "stylus"
+        ]
+    },
+    {
+        numb: 6,
+        question: "An electronic device that process data, converting it into information?",
+        answer: "computer",
+        options: [
+            "computer",
+            "processor",
+            "case",
+            "stylus"
+        ]
+    },
+];
+  
