3f35f5302fe2078a29bdff18f4b7b5fa0542ef47
Suzan Dewitz <suzanyaa@yahoo.com>
2022-12-16 19:32:22 +0000

    Icon change

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,8 +104,8 @@ function showQuestions(index){
 }
 
 // div tags for icons
-let checkIconTag = '<div class="icon check"><i class="fa-sharp fa-solid fa-check"></i></div>';
-let xmarkIconTag = '<div class="icon xmark"><i class="fa-solid fa-x"></i></div>';
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 // if user clicked on option
 function optionSelected(answer){
